c5b1d219bcef913f750d609ede68165192ae9b70
simon <simon@dancingcloudservices.com>
2022-06-22 16:46:29 -0600

    Day 2 14:45 counter speed lab 2

--- src/main/java/runnables/Counter2.java
+++ src/main/java/runnables/Counter2.java
@@ -1,14 +1,22 @@
 package runnables;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 class MyCounter2 implements Runnable {
-  public volatile long count = 0;
+  public /*volatile*/ long count = 0;
   private Object rendezvous = new Object();
+  ReentrantLock rl = new ReentrantLock();
 
   @Override
   public void run() {
     for (int i = 0; i < 1_000_000_000; i++) {
-      synchronized (rendezvous) {
+//      synchronized (rendezvous) {
+      rl.lock();
+      try {
         count++;
+      }
+      finally {
+        rl.unlock();
       }
     }
   }
@@ -31,3 +39,15 @@ public class Counter2 {
     System.out.printf("time taken: %7.4f\n", (time / 1_000_000_000.0));
   }
 }
+/*
+Ubuntu 22.04 Linux
+naked var 0.0929 s
+volatile var 15.8771 s
+synchronized 74.7545 s
+ReentrantLock 43.0698 s
+
+Semaphore
+AtomicInteger
+LongAdder
+LongAccumulator
+ */
